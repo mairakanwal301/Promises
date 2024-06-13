e2992f995c506a325fc6108b49504977438bf35b
mairarafiq301 <mairarafiq301@gmail.com>
2024-06-13 10:54:25 -0700

    first commit

---
+++ async.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let sirZiaPromise1 = new Promise((resolve, reject) => {
+    resolve("congrats! You passes lets get a party.");
+});
+sirZiaPromise1.then((response) => {
+    console.log(response);
+});
+let sirZiaPromise2 = new Promise((resolve, reject) => {
+    reject("Unfortunately! You are rejected.");
+});
+sirZiaPromise2.catch((response) => {
+    console.log(response);
+    // pattern2
+});
+let sirZiaPromise = new Promise((reselove, reject) => {
+    reject("mai nh jarha");
+});
+async function sirZiaPromiseStatus() {
+    try {
+        let status = await sirZiaPromise;
+        console.log(status);
+    }
+    catch {
+        console.log("failed");
+    }
+}
+sirZiaPromiseStatus();
